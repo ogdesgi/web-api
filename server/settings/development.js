e5bed260f44aca8edd0dca25cfadeb43ef3a626a
Julien Sarazin <julien.sarazin@me.com>
2015-12-09 15:29:26 +0100

    updated settings and added session storage

--- server/settings/development.js
+++ server/settings/development.js
@@ -1,15 +1,6 @@
 module.exports = {
     port: 8080,
-    db: 'mongodb://localhost:27017/coderpower-api-v3',
-    sessionDb: 'mongodb://localhost:27017/coderpower-dev-session-db',
-    cache: {
-        host: '127.0.0.1',
-        port: 6379,
-        password: '',
-        expiration: 50
-    },
-    logger:{
-        target: 'console',
-        directory: ''
-    }
+    db: 'mongodb://localhost:27017/todo-list-api',
+    sessionDb: 'mongodb://localhost:27017/todo-list-session-db',
+    sessionTTL: 2 * 24 * 60 * 60
 };
